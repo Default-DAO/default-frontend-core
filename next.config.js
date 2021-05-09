71494c48d124f4395564fdc8429ee9da26b78ba0
zaqk <zachking1218@gmail.com>
2021-05-09 20:43:02 +0300

    temp debug lines

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 require('dotenv').config({path: __dirname + '/.env'})
 
+console.log(`next.config.js`);
+console.log(`NODE_ENV===${process.env.NODE_ENV}`);
+console.log(`API_URL===${process.env.API_URL}`);
+
 module.exports = {
   env: {
     API_URL: process.env.API_URL
